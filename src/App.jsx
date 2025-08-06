6ff559f284c39708e80475bd741c0ffd0c2bb14d
EstebanCaponio <estebancaponio@gmail.com>
2025-08-06 12:53:35 +0200

    feat: milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
 
   const products = [
@@ -7,14 +9,46 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  const [addedProd, setAddedProd] = useState([]);
+  // console.log(addedProd)
+
+  const addToCart = product => {
+    const isProductAlreadyAdded = addedProd.some(p => p.name === product.name);
+    if (isProductAlreadyAdded) {
+      return;
+    }
+
+    setAddedProd(curr => [...curr, {
+      ...product,
+      quantity: 1
+    }])
+  }
 
   return (
     <>
       <div>
-        {products.map(p => (
-          <p key={p.name}>{p.name}: {p.price}</p>
+        <h1>Prodotti:</h1>
+        {products.map((p, i) => (
+          <>
+            <div key={i} className="container">
+              <p>{p.name}: {p.price.toFixed(2)}€</p>
+              <button onClick={() => addToCart(p)}> aggiungi al carrello</button>
+            </div>
+          </>
         ))}
       </div>
+
+      {addedProd.length > 0 && (<>
+        <h2>Carrello:</h2>
+        <ul>
+          {addedProd.map((p, i) => (
+            <li key={i}>
+              <p>{p.quantity} x {p.name} {p.price.toFixed(2)}€</p>
+            </li>
+          ))}
+        </ul>
+      </>
+      )}
     </>
   )
 }
